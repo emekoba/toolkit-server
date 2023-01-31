33cb1ecd1be44107abea49e2c330eb19474fc2e3
emekoba <rjemekoba@gmail.com>
2023-01-31 08:16:56 +0100

    created verify and generate funct in bloc file

---
+++ bloc.js
@@ -0,0 +1,57 @@
+const https = require("https");
+require("dotenv").config();
+
+let APIKEY = `Bearer ${process.env.PAYSTACK_APIKEY}`;
+
+function generate() {
+	const params = JSON.stringify({
+		email: "rjemekoba@email.com",
+		amount: 237500,
+	});
+
+	const options = {
+		hostname: "api.paystack.co",
+		port: 443,
+		path: "/transaction/initialize",
+		method: "POST",
+		headers: {
+			Authorization: APIKEY,
+			"Content-Type": "application/json",
+		},
+	};
+
+	const req = https
+		.request(options, (res) => {
+			let data = "";
+			res.on("data", (chunk) => (data += chunk));
+			res.on("end", () => console.log(JSON.parse(data)));
+		})
+		.on("error", (error) => console.error(error));
+
+	req.write(params);
+	req.end();
+}
+
+function verify(ref) {
+	const options = {
+		hostname: "api.paystack.co",
+		port: 443,
+		path: `/transaction/verify/${ref}`,
+		method: "GET",
+		headers: {
+			Authorization: APIKEY,
+		},
+	};
+
+	const req = https
+		.request(options, (res) => {
+			let data = "";
+			res.on("data", (chunk) => (data += chunk));
+			res.on("end", () => console.log(JSON.parse(data)));
+		})
+		.on("error", (error) => console.error(error));
+
+	req.end();
+}
+
+module.exports = { generate, verify };
--- index.js
+++ index.js
@@ -1,41 +1,15 @@
 const express = require("express");
 const app = express();
-const https = require("https");
+const { generate, verify } = require("./bloc");
 
 const port = 1300;
-let APIKEY = "Bearer sk_test_3e1985b417a1687392580aa3580f6da0ceb7cc77";
-
-function generate() {
-	const params = JSON.stringify({
-		email: "customer@email.com",
-		amount: "20000",
-	});
-
-	const options = {
-		hostname: "api.paystack.co",
-		port: 443,
-		path: "/transaction/initialize",
-		method: "POST",
-		headers: {
-			Authorization: APIKEY,
-			"Content-Type": "application/json",
-		},
-	};
-
-	const req = https
-		.request(options, (res) => {
-			let data = "";
-			res.on("data", (chunk) => (data += chunk));
-			res.on("end", () => console.log(JSON.parse(data)));
-		})
-		.on("error", (error) => console.error(error));
-
-	req.write(params);
-	req.end();
-}
 
 app.get("/generate", async (req, res) => {
 	generate();
+});
+
+app.get("/verify/:ref", async (req, res) => {
+	verify(req.params.ref);
 });
 
 app.listen(port, () => {
